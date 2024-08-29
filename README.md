0499a98dd89ce1d3e3d18716a249f1c35ad8f3b7
Mohamed Mahmodi <103428364+MohammedMMC@users.noreply.github.com>
2024-08-29 16:44:03 +0300

    2. commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Rock Paper Scissors Game
+
+This is a classic Rock-Paper-Scissors game where you compete against the computer. The game is simple, quick, and fun. Choose Rock, Paper, or Scissors and see if you can outsmart the computer.
+
+## How to Play
+
+1. **Start the Game**: The player and the computer will each choose one of the three options: Rock, Paper, or Scissors.
+2. **Make Your Choice**: Select one of the following:
+   - **Rock**
+   - **Paper**
+   - **Scissors**
+3. **Game Logic**:
+   - **Rock beats Scissors**: Rock crushes Scissors.
+   - **Scissors beats Paper**: Scissors cuts Paper.
+   - **Paper beats Rock**: Paper covers Rock.
+   - If both choose the same option, it's a **Draw**.
+4. **Play Again**: Continue playing to see how many rounds you can win!
+
+You can play it on https://mohammedmmc.github.io/RPS-Game
--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const messageElement = document.getElementById("message");
+const pcomElement = document.getElementById("pcom");
+const pyouElement = document.getElementById("pyou");
+
+let pcom = 0;
+let pyou = 0;
 
 function select(s) {
     messageElement.style.display = "none";
     const PC_CHOICE = Math.floor(Math.random() * 3) + 1;
     messageElement.style.display = "block";
+    messageElement.style.color = "white";
     messageElement.innerText = ".........";
+    document.querySelectorAll("button").forEach(e => e.disabled = true);
     setTimeout(() => {
-        messageElement.innerText = s === PC_CHOICE ? "Draw!" ;
-    });
+        const status = s === PC_CHOICE ? 3 : (s - PC_CHOICE + 3) % 3 === 1 ? 1 : 2;
+
+        if (status === 1) {
+            // WIN
+            messageElement.innerText = "You won the round!";
+            messageElement.style.color = "lime";
+            pyou++;
+        } else if (status === 2) {
+            // LOOSE
+            messageElement.innerText = "You lost the round!";
+            messageElement.style.color = "red";
+            pcom++;
+        } else if (status === 3) {
+            // DRAW
+            messageElement.innerText = "Draw!";
+            messageElement.style.color = "grey";
+        }
+        pcomElement.innerText = pcom;
+        pyouElement.innerText = pyou;
+    document.querySelectorAll("button").forEach(e => e.disabled = false);
+}, 1000);
 }
